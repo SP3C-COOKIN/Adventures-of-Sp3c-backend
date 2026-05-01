a5940155f34d0526546f103f1638bfc52b75ef89
Yatharth <sp3ccyy@gmail.com>
2026-05-01 15:31:18 +0000

    feat: add like system and extended comment schema

--- comment.js
+++ comment.js
@@ -1,9 +1,26 @@
 const mongoose = require("mongoose");
 
 const commentSchema = new mongoose.Schema({
-  author: String,
-  text: String,
-  timestamp: { type: Date, default: Date.now }
+  author: {
+  type: String,
+  required: true,
+  trim: true,
+  minlength: 1,
+  maxlength: 20
+},
+  text: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1,
+    maxlength: 300
+  },
+  timestamp: { type: Date, default: Date.now },
+  like: {
+    type: number,
+    default: 0,
+    min: 0,
+  }
 });
 
 module.exports = mongoose.model("Comment", commentSchema);
--- server.js
+++ server.js
@@ -3,7 +3,6 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const Comment = require("./Comment");
-
 const app = express();
 
 app.use(express.json());
@@ -15,6 +14,28 @@ mongoose.connect(process.env.MONGO_URI)
 app.get("/comments", async (req, res) => {
   const comments = await Comment.find();
   res.json(comments);
+});
+
+app.put("`/comments/${comments._id}/Like`", async (req, res) => {
+  const { action } = req.body;
+
+  if (!author || !text) {
+    res.sendStatus(400).send("Missing Fields")
+  }
+  if (author.length > 30) {
+    res.sendStatus(400).send("Author Name too long")
+  }
+  if (author.length > 300) {
+    res.sendStatus(400).send("Comment's valid length exceeded")
+  }
+  const change = action === "increment" ? 1 : -1;
+  
+  await comment.findByIdAndUpdate(
+  req.params.id,
+  { $inc: {like : change}}
+  );
+  res.sendStatus(200);
+
 });
 
 app.post("/comments", async (req, res) => {
